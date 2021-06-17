9652d46c41724351f2f53bfc700f37a668a5c072
Prateek Kumar Prateek Kumar <A00218636@mycambrian.ca>
2021-06-17 06:25:31 +0530

    Add project files.

---
+++ WebLAB1/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebLAB1.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+        public int EmployeeID { get; set; }
+        [Required,FromQuery(Name = "Name")]
+       
+        public string Name { get; set; }
+        [Required,FromQuery(Name = "Department")]
+        public string Department { get; set; }
+        [Required,FromQuery(Name = "Designation")]
+        public string Designation { get; set; }
+        public string Result { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Result = Name + " " + Department + " " + Designation;
+            //return Result;
+        }
+    }
+}
---
+++ WebLAB1/Pages/Intro.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebLAB1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        [FromQuery(Name ="Operand1")]
+        public double Operand1 { get; set; }
+        [FromQuery(Name = "Operand2")]
+       public double Operand2 { get; set; }
+        //[FromQuery(Name = "Result")]
+        public  double Result { get; set; }
+        public void OnGet()
+        {
+            Result = Operand1 * Operand2;
+
+           // return Result;
+        }
+
+
+        public double MultiplyOperation(double x, double y)
+        {
+            Result = x * y;
+
+            return Result;
+        }
+    }
+}
